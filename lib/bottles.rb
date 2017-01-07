2201ffcf04738819c32288c313f86ad4cb452871
Apostolis Stergiou <apostergiou@gmail.com>
2017-01-07 21:43:56 +0200

    Refactor bottles.rb

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,31 +4,54 @@ class Bottles
   end
 
   def verses(starting, ending)
-    starting.downto(ending).map {|i| verse(i)}.join("\n")
+    starting.downto(ending).collect { |i| verse(i) }.join("\n")
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, " +
-        "no more bottles of beer.\n" +
-        "Go to the store and buy some more, " +
-        "99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
-        "Take it down and pass it around, " +
-        "no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n"
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
+
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} " \
+       'of beer on the wall, ' \
+       "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" \
+       "#{bottle_number.action}, " \
+       "#{next_bottle_number.quantity} #{next_bottle_number.container}" \
+       " of beer on the wall.\n"
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def container
+    if number == 1
+      'bottle'
     else
-      "#{number} bottles of beer on the wall, " +
-        "#{number} bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "#{number-1} bottles of beer on the wall.\n"
+      'bottles'
     end
+  end
+
+  def quantity
+    number.zero? ? 'no more' : number.to_s
+  end
+
+  def action
+    if number.zero?
+      'Go to the store and buy some more'
+    else
+      "Take #{pronoun} down and pass it around"
+    end
+  end
+
+  def pronoun
+    number == 1 ? 'it' : 'one'
+  end
+
+  def successor
+    number.zero? ? 99 : number - 1
   end
 end
