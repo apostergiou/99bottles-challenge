ece3d80b42582151eb1ce6cc1fb50627d4ae8478
Apostolis Stergiou <apostergiou@gmail.com>
2017-04-13 15:26:57 +0300

    Refactor bottles.rb

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,50 +8,77 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container} " \
-       'of beer on the wall, ' \
-       "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" \
-       "#{bottle_number.action}, " \
-       "#{next_bottle_number.quantity} #{next_bottle_number.container}" \
-       " of beer on the wall.\n"
+    "#{bottle_number} of beer on the wall, ".capitalize +
+      "#{bottle_number} of beer.\n" \
+      "#{bottle_number.action}, " \
+      "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
 class BottleNumber
-  attr_reader :number
+  def self.for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    else
+      BottleNumber
+    end.new(number)
+  end
 
+  attr_reader :number
   def initialize(number)
     @number = number
   end
 
+  def to_s
+    "#{quantity} #{container}"
+  end
+
   def container
-    if number == 1
-      'bottle'
-    else
-      'bottles'
-    end
+    'bottles'
   end
 
   def quantity
-    number.zero? ? 'no more' : number.to_s
+    number.to_s
   end
 
   def action
-    if number.zero?
-      'Go to the store and buy some more'
-    else
-      "Take #{pronoun} down and pass it around"
-    end
+    "Take #{pronoun} down and pass it around"
   end
 
   def pronoun
-    number == 1 ? 'it' : 'one'
+    'one'
   end
 
   def successor
-    number.zero? ? 99 : number - 1
+    BottleNumber.for(number - 1)
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    'no more'
+  end
+
+  def action
+    'Go to the store and buy some more'
+  end
+
+  def successor
+    BottleNumber.for(99)
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    'bottle'
+  end
+
+  def pronoun
+    'it'
   end
 end
